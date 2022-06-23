676cb96d2aa10a89a30625bab060d2e084743ba6
Behruz17 <71710568+Behruz17@users.noreply.github.com>
2022-06-23 16:45:59 +0500

    write reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let arr = Math.abs(n).toString().split('');
+  let reversedArr = arr.reverse();
+  let result = reversedArr.join('');
+  return result;
 }
